ce9ad6ceca0d2d9165c82280b53fa70f8e1393dd
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-10 20:47:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function destructivelyRemoveFirstKitten() {
 
 
   kittens = ["Milo", "Otis", "Garfield"];
-  var appendKitten(name) = kittens.concat("Broom");
+  var appendKitten() = kittens.concat("Broom");
   console.log(kittens);
