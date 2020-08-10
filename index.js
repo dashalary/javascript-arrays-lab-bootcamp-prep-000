52d4e2673cd4ef2a6190371e0afe6f0c54bec1de
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-10 21:09:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-return [...kittens, name]
+return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return 
 }
